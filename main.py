f33b3c77b1a9b471c0d3fc595caa4f437f3b20d8
fbettega <francois.bettega@gmail.com>
2025-02-26 08:25:20 +0100

    loop to dl 2 times

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ if Rerun_alll:
 cards = Base_data_method.parse_large_json(os.path.join(json_dir,"all_cards.json"))
 
 # Warning need more than one run to DL all cards ?
-Base_data_method().download_card_images(cards,images_dir,bad_images_dir)
+for i in [0,1]:
+    Base_data_method().download_card_images(cards,images_dir,bad_images_dir)
 
 # Example of problamatic cards need to be handle : cards['0001a521-655c-432f-9f52-1c199b792f68']
 # new example cards['000cb74e-2ebf-4c31-a334-4dbad1486ead']
