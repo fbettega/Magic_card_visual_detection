76091d4e66a2996ccec3d362bbe3e513b1c2f454
fbettega <francois.bettega@gmail.com>
2025-02-27 16:19:44 +0100

    test bug on my git

--- DL_and_genrate_data.py
+++ DL_and_genrate_data.py
@@ -15,7 +15,7 @@ cards = Base_data_method.parse_large_json(os.path.join(json_dir,"all_cards.json"
 # Warning need more than one run to DL all cards ?
 for i in [0,1]:
     Base_data_method().download_card_images(cards,images_dir,bad_images_dir)
-
+# test debug git
 # Example of problamatic cards need to be handle : cards['0001a521-655c-432f-9f52-1c199b792f68']
 # new example cards['000cb74e-2ebf-4c31-a334-4dbad1486ead']
 # Détection et déplacement des dos de cartes
